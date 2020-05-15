01df1cdd3e3de579466e88684823b791e389df9c
Alfian Losari <alfianlosari@gmail.com>
2020-05-15 11:25:45 +0800

    initial commit

---
+++ ExpenseTrackertvOS/Views/DashboardView.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardView.swift
+//  ExpenseTrackertvOS
+//
+//  Created by Alfian Losari on 10/05/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct DashboardView: View {
+    
+    var body: some View {
+        Text("Dashboard")
+    }
+
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
---
+++ ExpenseTrackertvOS/Views/LogView.swift
@@ -0,0 +1,24 @@
+//
+//  LogView.swift
+//  ExpenseTrackertvOS
+//
+//  Created by Alfian Losari on 10/05/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+import SwiftUI
+import CoreData
+
+struct LogView: View {
+  
+    var body: some View {
+        Text("Expenses")
+    }
+}
+
+
+struct LogView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogView()
+    }
+}
